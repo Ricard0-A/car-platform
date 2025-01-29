30375b219e2c78aa704eb99df7ebf72f2385b895
Ricard0-A <ricardoarias4@hotmail.com>
2025-01-29 03:23:04 +0000

    New ContactUs Page

--- src/front/js/pages/ContactUs.jsx
+++ src/front/js/pages/ContactUs.jsx
@@ -1,35 +1,32 @@
 import React from "react";
-
-
+import "../../styles/contact-us.css"
 
 const ContactUs = () => {
 
     const background = {
         position: "relative",
         height: "60vh",
-
     }
-    const colMod = {
-        left: "10rem",
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "start",
-    }
-
 
     return (
         <div className="container" style={{ marginTop: "100px" }}>
             <div className="row" style={background}>
-                <div className="col-6" style={colMod}>
+                <div className="col-12 col-md-6 info-bg" >
                     <h2>Contact Us</h2>
+                    <br />
                     <h5>You will receive a response during our <br />
                         business hours, Monday through <br /> Saturday</h5>
                 </div>
-                <div className="col-6">A</div>
+                <div className="col-12 col-md-6 form-bg">
+                    <form action="">
+                        <input className="w-75" type="text" placeholder="Full Name" />
+                        <input className="w-75" type="text" placeholder="Email" />
+                        <textarea className="w-75" name="message" id="" placeholder="Message"></textarea>
+                    </form>
+                    <button className="w-75 btn btn-success">Send</button>
+
+                </div>
             </div>
-            {/* <p> Hola! Entiendo que estás buscando una manera de controlar el tamaño y el comportamiento del texto dentro de un elemento, especialmente cuando se trata de saltos de línea.</p> */}
         </div>
     )
 }
